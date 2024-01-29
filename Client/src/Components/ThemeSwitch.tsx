fa5da8289fb7095a0b41763f32a8987bfb44229a
ozzlamaz <a.osman4233@gmail.com>
2024-01-29 23:33:02 +0200

    Refactor: Change ThemeSwitcher Style

--- Client/src/Components/ThemeSwitch.tsx
+++ Client/src/Components/ThemeSwitch.tsx
@@ -1,15 +1,17 @@
+import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
 
 const ThemeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
     <HStack>
+      <SunIcon color={colorMode === "dark" ? "grey" : undefined} />
       <Switch
-        colorScheme="green"
+        colorScheme="blue"
         isChecked={colorMode === "dark"}
         onChange={toggleColorMode}
       />
-      <Text whiteSpace="nowrap">Dark Mode</Text>
+      <MoonIcon color={colorMode === "light" ? "grey" : undefined} />
     </HStack>
   );
 };
