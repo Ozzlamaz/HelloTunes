c2d489a02b6f44d4c29f133ac01b9a3c1c46b05e
ozzlamaz <a.osman4233@gmail.com>
2024-02-19 18:32:40 +0200

    added carousel overlay

--- Client/src/Components/AlbumsCarousel.tsx
+++ Client/src/Components/AlbumsCarousel.tsx
@@ -1,7 +1,8 @@
-import { Heading, Image } from "@chakra-ui/react";
+import { Heading } from "@chakra-ui/react";
 import Carousel from "./Carousel";
 import useAlbums from "../hooks/useAlbums";
 import useFilterQueryStore from "../filterquery/store";
+import ItemCarousel from "./ItemCarousel";
 
 const AlbumsCarousel = () => {
   const filterQuery = useFilterQueryStore((s) => s.filterQuery);
@@ -12,9 +13,9 @@ const AlbumsCarousel = () => {
       <Heading marginBottom={3} as={"h1"}>
         New Albums
       </Heading>
-      <Carousel>
+      <Carousel pauseOnHover>
         {data?.albums.items.map((item) => (
-          <Image borderRadius={20} key={item.id} src={item.images[0].url} />
+          <ItemCarousel item={item} />
         ))}
       </Carousel>
     </>
