b1c60030211e8f48e1490c9225f117bce434e3e0
ozzlamaz <a.osman4233@gmail.com>
2024-06-10 19:45:11 +0300

    switch TopResult with DetailsCard

--- Client/src/Pages/SearchPage.tsx
+++ Client/src/Pages/SearchPage.tsx
@@ -1,13 +1,13 @@
-import { Grid, GridItem } from "@chakra-ui/react";
+import { Box, Grid, GridItem } from "@chakra-ui/react";
 import useItems from "../hooks/useItems";
 import { useParams } from "react-router-dom";
-import TopResult from "../Components/SearchPageComponents/TopResult";
 import Section from "../Components/Containers/Section";
 import Heading1 from "../Components/Headings/Heading1";
 import ResponseErrorComp from "../Components/Error/ResponseErrorComp";
 import TracksResults from "../Components/SearchPageComponents/TracksResults";
 import AlbumsResults from "../Components/SearchPageComponents/AlbumsResults";
 import ArtistResults from "../Components/SearchPageComponents/ArtistResults";
+import ArtistDetailsCard from "../Components/Details/ArtistDetailsCard";
 
 const SearchPage = () => {
   const params = useParams();
@@ -17,23 +17,18 @@ const SearchPage = () => {
 
   return (
     <Section>
-      <Heading1 isLoading={isLoading}>{params.q}</Heading1>
-      <Grid gridGap={5} templateColumns={"1fr 1fr"}>
-        <GridItem colSpan={{ base: 2, lg: 1 }}>
-          <TopResult isLoading={isLoading} item={data?.artists.items[0]} />
-        </GridItem>
-        <GridItem
-          colSpan={{ base: 2, lg: data?.artists.items.length === 0 ? 2 : 1 }}
-        >
-          <TracksResults tracks={data?.tracks.items} isLoading={isLoading} />
-        </GridItem>
-        <GridItem colSpan={2}>
-          <AlbumsResults albums={data?.albums.items} isLoading={isLoading} />
-        </GridItem>
-        <GridItem colSpan={2}>
-          <ArtistResults artists={data?.artists.items} isLoading={isLoading} />
-        </GridItem>
-      </Grid>
+      <Heading1
+        isLoading={isLoading}
+      >{`You searched for: ${params.q}`}</Heading1>
+      <Box>
+        <ArtistDetailsCard
+          isLoading={isLoading}
+          artist={data?.artists.items[0]}
+        />
+        <TracksResults tracks={data?.tracks.items} isLoading={isLoading} />
+        <AlbumsResults albums={data?.albums.items} isLoading={isLoading} />
+        <ArtistResults artists={data?.artists.items} isLoading={isLoading} />
+      </Box>
     </Section>
   );
 };
